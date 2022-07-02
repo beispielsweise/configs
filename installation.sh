c4b6ad305a74b4811ae01b6e426d1abb0e90de82
waste211 <egputilin@gmail.com>
2022-07-02 18:56:20 +0200

    modified:   installation.sh

--- installation.sh
+++ installation.sh
@@ -1,43 +1,91 @@
 #!/bin/bash
 
-install_all_files () {
+install_files () {
+	if [ ch == 0 ]
+	then
+		echo "OK"
+	fi
+
+
+	# ------------------------ CONFIGS -----------------------
+	# .zshrc
 	cp config-files/.zshrc ~/
 	if [ $? == 0 ] 
 	then
-		echo "[+]${LBLUE} .zshrc succesfully moved\n"
+		echo -e "[+]${LBLUE} .zshrc succesfully copied${NC}"
 	else
-		echo "[-]${RED} something went wrong while copying .zshrc\n"
+		echo -e "[-]${RED} something went wrong while copying .zshrc${NC}"
 	fi
 	
+	# .bashrc
 	cp config-files/.bashrc ~/
 	if [ $? == 0 ] 
 	then
-		echo "[+]${LBLUE} .bashrc succesfully moved\n"
+		echo -e "[+]${LBLUE} .bashrc succesfully copied${NC}"
 	else
-		echo "[-]${RED} something went wrong while copying .bashrc\n"
+		echo -e "[-]${RED} something went wrong while copying .bashrc${NC}"
 	fi
 
+	# .gitconfig
 	cp config-files/.gitconfig ~/
 	if [ $? == 0 ] 
 	then
-		echo "[+]${LBLUE} .gitconfig succesfully moved\n"
+		echo -e "[+]${LBLUE} .gitconfig succesfully copied${NC}"
 	else
-		echo "[-]${RED} something went wrong while copying .gitconfig\n"
+		echo -e "[-]${RED} something went wrong while copying .gitconfig${NC}"
 	fi
 
-	if [ ! -d "~/.config/nvim/" ] 
-		if [ ! -d "~/.config/" ] 
-			echo "[-]${RED}"
+	# init.vim
+	if [[ ! -d "~/.config/nvim/" && "~/.config/nvim/" =~ ^\. ]]
+	then
+		echo -e "[-]${RED} No directory path \"~/.config/nvim\", which is essential for NEOVIM configuration\n
+Please, check if all necessary folders exist${NC}"
+	else
+		cp config-files/init.vim ~/.config/nvim/
+		if [ $? == 0 ] 
+		then
+			echo -e "[+]${LBLUE} init.vim succesfully copied${NC}"
+		else
+			echo -e "[-]${RED} something went wrong while copying init.vim${NC}"
 		fi
 	fi
-	cp config-files/init.vim
+
+	# alacritty.yml 
+	if [[ ! -d "~/.config/alacritty/" && "~/.config/alacritty/" =~ ^\. ]]
+	then
+		echo -e "[-]${RED} No directory path \"~/.config/alacritty/\", which is essential for NEOVIM configuration\n
+		Please, check if all necessary folders exist${NC}"
+	else
+		cp config-files/alacritty.yml ~/.config/alacritty/
+		if [ $? == 0 ] 
+		then
+			echo -e "[+]${LBLUE} alacritty.yml succesfully copied${NC}"
+		else
+			echo -e "[-]${RED} something went wrong while copying alacritty.yml${NC}"
+		fi
+	fi
+	
+	# ------------------------ SCRIPTS -----------------------
+	# nes_o.sh
+	echo -e "${GREEN}Sudo commands folowing:${NC}"
+	sudo cp shell-scripts/nes_o.sh /bin/
 	if [ $? == 0 ] 
 	then
-		echo "[+]${LBLUE} .zshrc succesfully moved\n"
+		echo -e "[+]${LBLUE} script nes_o.sh succesfully copied${NC}"
 	else
-		echo "[-]${RED} something went wrong while copying .zshrc\n"
+		echo -e "[-]${RED} something went wrong while copying script nes_o.sh${NC}"
+	fi
+
+	# nvim_swp_clear.sh
+	sudo cp shell-scripts/nvim_swp_clear.sh /bin/
+	if [ $? == 0 ] 
+	then
+		echo -e "[+]${LBLUE} script nvim_swp_clear.sh succesfully copied${NC}"
+	else
+		echo -e "[-]${RED} something went wrong while copying script nvim_swp_clear.sh${NC}"
 	fi
 }
+
 
 #-------------------------CONSTANTS---------------------
 RED='\033[0;31m'		# red 
@@ -61,29 +109,30 @@ To install:
 echo -e "${CYAN}\nIt's time to choose: ${NC}"
 read ch
 
+clear
+echo -e "${GREEN}Installation proces began...${NC}\n\n"
+
 case $ch in
 	0) 
-	echo "ok"
-	install_all_files
+	install_files
+	echo -e "\n\n${LBLUE}All files installed.\n 
+	\t${RED}!!!!!!!!!!!! YOU NEED TO SET CHMOD +X FOR ALL SCRIPTS !!!!!!!!!!!! ${NC}"
 	;;
 
 	1)
-	echo "ok2"
+	echo -e "$\n\n{LBLUE}All config files are installed${NC}"
 	;;
 
 	2)
-	echo "ok3"
 	;;
 
 	3)
-	echo "ok4"
 	;;
 
 	4)
-	echo "ok5"
 	;;
 
 	?)
-	echo "[-]${CYAN} Man, read the desciption again, to whom did I write it, huh?"
+	echo -e "[-]${CYAN} Man, read the desciption again, to whom did I write it, huh?"
 	;;
 esac
