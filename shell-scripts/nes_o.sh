1dc21618608b6f359e734e891a34b733305fb81d
waste <59206800+waste211@users.noreply.github.com>
2022-09-13 01:14:06 -0700

    Add files via upload

---
+++ shell-scripts/nes_o.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+
+helpFunction()
+{
+   echo "Usage: Creating .nes files [ -d sourceDirectory ] [ -c ConfigFile ] [ -o OutputFile ] [ -r ] [ -a ] [ -h ] 
+
+This script creates an \".o\" output file, compiling a whole NES game project
+
+Usage:
+-d		With these parameter you specify a directory, in which all the source files are located 
+		( Usually it is a /src folder ). 
+			E.g: -d some_project/src		OR		-d some_project/src/
+		Handles both styles
+-c		Here is a path to a configuration file
+			E.g: -c path/to/file/example.cfg			OR			-c example.cfg
+-o		a name of output .nes file, can be specified both with ot without path, depends on needs
+			E.g: -o path/to/file/output		OR		-o output.nes
+-r		Specify, if want to remove all created .o files ( usually not in use, made just in case )
+-a		To see, what commands are runned ( echoing them )
+-h		For help"
+
+   exit 1 # Exit script after printing help
+}
+
+
+# get all parameters
+removeOFiles=0
+showCommands=0
+while getopts "d:c:o:rah" opt
+do
+	case "$opt" in
+		d ) asmDirectory="$OPTARG" ;;
+		c ) config="$OPTARG" ;;
+		o ) outputFileName="$OPTARG" ;;
+		r ) removeOFiles=1 ;;
+		a ) showCommands=1 ;;
+		h ) helpFunction ;;
+		? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+	esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$asmDirectory" ] || [ -z "$config" ]|| [ -z "$outputFileName" ] ; then
+	echo "Some of the required parameters are missing. use \"nes_o.sh -h\" to see help"
+	exit 2
+fi
+
+
+# cut the last '/' character if present
+lengthWithoutSlash=$((${#asmDirectory}-1))
+if [ "${asmDirectory:lengthWithoutSlash}" == "/" ] ; then
+	asmDirectory=${asmDirectory::-1}
+fi
+
+# add .cfg if necessary
+configNameCut=${config%.cfg}
+if [ ${#config} == ${#configNameCut} ] ; then
+	config=$config.cfg
+fi
+
+# add .nes if necessary
+outputFileNameCut=${outputFileName%.nes}
+if [ ${#outputFileName} == ${#outputFileNameCut} ] ; then
+	outputFileName=$outputFileName.nes
+	echo $outputFileName
+fi
+
+
+# Begin script in case all parameters are correct
+startDirectory=$(pwd)
+cd $asmDirectory
+
+filesString=""
+asmFiles=$(find -name "*.asm")
+
+for file in $asmFiles ; do
+	fileName=${file:1:-4}.o
+	filesString="$filesString$asmDirectory$fileName "
+
+	ca65 $file
+	if [ $showCommands == 1 ] ; then 
+		echo ca65 $file
+	fi 
+done
+
+cd $startDirectory
+
+ld65 $filesString -C $config -o $outputFileName
+nesCreated=$?
+if [ $showCommands == 1 ] ; then
+	echo ld65 $filesString -C $config -o $outputFileName
+fi 
+
+if [ $nesCreated == 0 ]
+then
+	echo	[+] .nes file successfully created
+else
+	echo	[-] an error occured, while creating a .nes file
+fi
+
+if [ $removeOFiles == 1 ] ; then
+	for file in $asmFiles ; do
+		rm "$asmDirectory${file:1:-4}.o"
+		if [ showCommands == 1 ] ; then 
+			echo rm $asmDirectory${file:1:-4}.o
+		fi 
+	done
+	echo "[+] .o files are deleted"
+fi
