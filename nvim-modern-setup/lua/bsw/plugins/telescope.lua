9962906c98d0b719cc5dcf2d9196426981e765e2
beispielsweise <59206800+beispielsweise@users.noreply.github.com>
2024-11-27 22:53:09 +0100

    Update telescope.lua

--- nvim-modern-setup/lua/bsw/plugins/telescope.lua
+++ nvim-modern-setup/lua/bsw/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    -- navigate to .local/.../telescope-fzf-native.nvim, run either the command below, or just "make"
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
